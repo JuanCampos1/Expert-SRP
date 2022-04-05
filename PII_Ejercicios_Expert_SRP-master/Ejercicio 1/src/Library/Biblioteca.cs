e661e7d2673b6ab17e3eaace59515b7af5b063fa
JuanCampos1 <88661667+JuanCampos1@users.noreply.github.com>
2022-04-05 20:38:53 -0300

    1

---
+++ PII_Ejercicios_Expert_SRP-master/Ejercicio 1/src/Library/Biblioteca.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SRP
+{
+    public class Biblioteca
+    {
+        /*Se crea una nueva clase biblioteca, la cual tiene la responsabilidad de conocer en que estante y
+        sector se almacena el libro*/
+        public string SectorBiblioteca { get ; set; }
+        public string EstanteBiblioteca { get ; set; }
+
+        public void AlmacenarLibro(Libro libro, String sector, String estante)
+        {
+            this.SectorBiblioteca = sector;
+            this.EstanteBiblioteca = estante;
+        }
+    }
+}
--- PII_Ejercicios_Expert_SRP-master/Ejercicio 1/src/Library/Libro.cs
+++ PII_Ejercicios_Expert_SRP-master/Ejercicio 1/src/Library/Libro.cs
@@ -4,24 +4,19 @@ namespace SRP
 {
     public class Libro
     {
+        /*Se mueve el método AlmacenarLibro a una clase biblioteca ya que el libro no tiene
+        la necesidad de conocer en donde se almacena*/
 
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
-        public string SectorBiblioteca { get ; set; }
-        public string EstanteBiblioteca { get ; set; }
+        
 
         public Libro(String titulo, String autor, String codigo)
         {
             this.Titulo = titulo;
             this.Autor = autor;
             this.Codigo = codigo;
-        }
-
-        public void AlmacenarLibro(String sector, String estante)
-        {
-            this.SectorBiblioteca = sector;
-            this.EstanteBiblioteca = estante;
         }
 
     }
