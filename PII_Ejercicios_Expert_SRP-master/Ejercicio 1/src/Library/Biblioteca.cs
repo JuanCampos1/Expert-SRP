921b8ed1e5b61428344fd9392dc6a493cce3ce2e
JuanCampos1 <88661667+JuanCampos1@users.noreply.github.com>
2022-04-07 20:02:25 -0300

    1

--- PII_Ejercicios_Expert_SRP-master/Ejercicio 1/src/Library/Biblioteca.cs
+++ PII_Ejercicios_Expert_SRP-master/Ejercicio 1/src/Library/Biblioteca.cs
@@ -5,7 +5,7 @@ namespace SRP
     public class Biblioteca
     {
         /*Se crea una nueva clase biblioteca, la cual tiene la responsabilidad de conocer en que estante y
-        sector se almacena el libro*/
+        sector se almacena el librao*/
         public string SectorBiblioteca { get ; set; }
         public string EstanteBiblioteca { get ; set; }
 
